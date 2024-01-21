176f153e26ffad3fe05c46b9c09a9253e9982476
Yizack Rangel <yizackr@gmail.com>
2024-01-21 08:53:36 -0500

    chore: session argument

--- server/api/auth/twitch.get.ts
+++ server/api/auth/twitch.get.ts
@@ -3,8 +3,8 @@ export default oauth.twitchEventHandler({
     emailRequired: true,
     scope: ["chat:read", "chat:edit", "channel:moderate", "moderator:read:chatters"]
   },
-  async onSuccess(event, user) {
-    await setUserSession(event, { user });
+  async onSuccess(event, session) {
+    await setUserSession(event, { user: session });
     return sendRedirect(event, "/");
   }
 });
