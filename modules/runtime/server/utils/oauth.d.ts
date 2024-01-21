465ab207e8c086c45225bceea5b152693b1fdd37
Ahmed Rangel <ahmedrangel@outlook.com>
2024-01-21 06:25:33 -0500

    feat: twitch login (initial)

---
+++ modules/runtime/server/utils/oauth.d.ts
@@ -0,0 +1,22 @@
+import { githubEventHandler } from '../lib/oauth/github';
+import { googleEventHandler } from '../lib/oauth/google';
+import { spotifyEventHandler } from '../lib/oauth/spotify';
+import { twitchEventHandler } from '../lib/oauth/twitch';
+import { auth0EventHandler } from '../lib/oauth/auth0';
+import { microsoftEventHandler } from '../lib/oauth/microsoft';
+import { discordEventHandler } from '../lib/oauth/discord';
+import { battledotnetEventHandler } from '../lib/oauth/battledotnet';
+import { keycloakEventHandler } from '../lib/oauth/keycloak';
+import { linkedinEventHandler } from '../lib/oauth/linkedin';
+export declare const oauth: {
+    githubEventHandler: typeof githubEventHandler;
+    spotifyEventHandler: typeof spotifyEventHandler;
+    googleEventHandler: typeof googleEventHandler;
+    twitchEventHandler: typeof twitchEventHandler;
+    auth0EventHandler: typeof auth0EventHandler;
+    microsoftEventHandler: typeof microsoftEventHandler;
+    discordEventHandler: typeof discordEventHandler;
+    battledotnetEventHandler: typeof battledotnetEventHandler;
+    keycloakEventHandler: typeof keycloakEventHandler;
+    linkedinEventHandler: typeof linkedinEventHandler;
+};
