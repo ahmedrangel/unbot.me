ef737e7ae9c2d27c6b3e3b5b8f3c004077de09fe
Ahmed Rangel <ahmedrangel@outlook.com>
2024-01-25 07:04:44 -0500

    typo

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@ const { loggedIn } = useUserSession();
       <div class="d-flex justify-content-center align-items-center">
         <div class="col-7">
           <h1 class="mb-5"><b>unbot.me</b></h1>
-          <h6 class="mb-5">Unbotme is a bot for Twitch designed to automatically ban identified bot users that persistently lurk in channels.</h6>
+          <h6 class="mb-5">Unbotme is a bot for Twitch designed to automatically ban identified bots that persistently lurk in channels.</h6>
           <PrimeButton type="button" label="JOIN MY CHANNEL" @click="navigateTo('/api/auth/twitch', { external: true })">
             <template #icon>
               <Icon class="iconify me-1" style="font-size: 24px; width: 40px;" name="simple-icons:twitch" />
