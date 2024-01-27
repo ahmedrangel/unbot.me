0c1295d0fd5265d278d1dfafcd8cd0693b3a6a48
Ahmed Rangel <ahmedrangel@outlook.com>
2024-01-27 07:51:28 -0500

    chore: adjust

--- pages/index.vue
+++ pages/index.vue
@@ -29,13 +29,13 @@ const { loggedIn } = useUserSession();
           <PrimeCard class="text-center h-100">
             <template #content>
               <h5 class="mb-3">{{ guide.title }}</h5>
-              <div class="text-start">
+              <div class="text-center">
                 <h6>
                   <!-- eslint-disable-next-line vue/no-v-html -->
                   <p v-html="guide.subtitle" />
                   <component :is="String(guide.list_type)">
                     <!-- eslint-disable-next-line vue/no-v-html -->
-                    <li v-for="(list, j) of guide.list" :key="j" class="mb-3" v-html="list" />
+                    <li v-for="(list, j) of guide.list" :key="j" class="mb-3 text-start" v-html="list" />
                   </component>
                   <img class="rounded" :src="guide.image" style="width: 100%; max-width: 300px;">
                 </h6>
